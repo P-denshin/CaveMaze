e89eeb40ad0df71fce9725d299c8f645142eb037
Secretary <s1300640@sendai-nct.jp>
2014-06-18 17:25:46 +0900

    MakingDelveMap_1

--- src/cavemaze/Map.java
+++ src/cavemaze/Map.java
@@ -9,6 +9,7 @@ public class Map {
      * @return マップ情報
      */
     public int[][] GetMap(){
+        return map;
     }
     
     /**
@@ -16,7 +17,31 @@ public class Map {
     * @param width  迷路の横幅
     * @throws cavemaze.Map.ImpossibilityException
     */
-    public void MakeMap() throws ImpossibilityException{
+    public void MakeMap() throws ImpossibilityException{   
+        //できない場合
+        if(height % 2 == 0 || width % 2 == 0){
+            throw new ImpossibilityException();
+        }
+        
+        map = new int[width][height];
+        
+        //すべて壁
+        for(int i = 0; i < width - 1; i++){
+            for(int j = 0; j < height -1; j++){
+                map[i][j] = -1;
+            }
+        }
+        
+        //一つごとに番号作成
+        int count = 0;
+        for(int i = 1; i < width - 1; i += 2){
+            for(int j = 1; j < height -1; j += 2){
+                map[i][j] = count;
+                count++;
+            }
+        }
+        
+        //迷路を作っていく
     }
     
     /**
@@ -26,6 +51,15 @@ public class Map {
      * @return 1で完成0は未完成
      */
     private int DelveMap(int x, int y){
+        
+        //完成判定
+        int tmp = map[1][1];
+        for(int i = 1; i < width -1; i++){
+            for(int j = 1; j < height -1; j++){
+                if(tmp != map[i][j] && map[i][j] != -1)
+                    return 0;
+            }
+        }
     }
     
     /**
@@ -33,16 +67,15 @@ public class Map {
     * @param py 自機のy座標
     */
     public void ShowMap(int px, int py){
-        for(int x = 0; x < map.length - 1; x++){
-            for(int y = 0; y < map[0].length - 1; y++){
-                System.out.print("");
-            }
-        }
     }
     
     public Map(int h, int w){
+        height = h; width = w;
     }
     
     class ImpossibilityException extends Exception {
+        public ImpossibilityException(){
+            super("作成される迷路は縦横の長さは奇数でなければ作れません。");
+        }
     }
 }
